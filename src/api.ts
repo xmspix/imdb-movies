2001adfc74ab11c203eadc999fa87d3a31571166
xmspix <mark.stoler.86@gmail.com>
2021-11-07 08:23:45 +0200

    update api

--- src/api.ts
+++ src/api.ts
@@ -12,25 +12,25 @@ export const createApiClient = (): ApiClient => {
     return {
         featuredMovies: () =>
             axios
-                .get(`http://mark-stoler.ddnsfree.com/imdb/featured-movies`)
+                .get(`https://stoler-imdb-api.herokuapp.com/featured-movies`)
                 .then((res: any) => res.data)
                 .catch((err: any) => err),
         topMovies: () =>
             axios
-                .get(`http://mark-stoler.ddnsfree.com/imdb/top-movies`)
+                .get(`https://stoler-imdb-api.herokuapp.com/top-movies`)
                 .then((res: any) => res.data)
                 .catch((err: any) => err),
         trailer: (id: string) =>
             axios
-                .get(`http://mark-stoler.ddnsfree.com/imdb/trailer/${id}`)
+                .get(`https://stoler-imdb-api.herokuapp.com/trailer/${id}`)
                 .then((res: any) => res.data)
                 .catch((err: any) => err),
         search: (term: string) =>
-            axios.get(`http://mark-stoler.ddnsfree.com/imdb/search/${term}`)
+            axios.get(`https://stoler-imdb-api.herokuapp.com/search/${term}`)
                 .then((res: any) => res.data)
                 .catch((err: any) => err),
         movieInfo: (id: string) =>
-        axios.get(`http://mark-stoler.ddnsfree.com/imdb/movie/${id}`)
+        axios.get(`https://stoler-imdb-api.herokuapp.com/movie/${id}`)
             .then((res: any) => res.data)
             .catch((err: any) => err)
     }
